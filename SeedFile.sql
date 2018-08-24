ca2b9f1ca3c27268a68bbc27fc49004329aad748
Adnan Yousef <yousef.eddy@gmail.com>
2018-08-23 19:13:48 -0500

    Wrote bamazonManager.js

--- SeedFile.sql
+++ SeedFile.sql
@@ -5,7 +5,7 @@ USE bamazon;
 CREATE TABLE products(
  item_id INT NOT NULL AUTO_INCREMENT,
  product_name VARCHAR(45) NOT NULL,
- department_name VARCHAR(45) NOT NULL,
+ department_name VARCHAR(45) DEFAULT NULL,
  price DECIMAL(10,2) DEFAULT 10.00,
  stock_quantity INT DEFAULT 0,
  PRIMARY KEY(item_id)
@@ -13,16 +13,16 @@ CREATE TABLE products(
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES
- ('MacBook', 'Electronics', 2000.00, 3),
- ('Surface Pro', 'Electronics', 1200.00, 5),
+ ('MacBook', 'Electronics', 2000.00, 2),
+ ('Surface Pro', 'Electronics', 1200.00, 4),
  ('PS4', 'Electronics', 350, 6),
- ('Xbox 1', 'Electronics', 400, 7),
+ ('Xbox 1', 'Electronics', 400, 3),
  ('A Smarter Way To Learn JavaScript', 'Books', 30, 5),
  ('Eloquent JavaScript', 'Books', 20, 10),
  ('JavaScript: The Good Parts', 'Books', 25, 8),
  ('Tool Set', 'Hardware', 50, 20),
  ('Light Bulbs', 'Hardware', 5, 50),
- ('Protein Bars', 'Food', 10, 10),
+ ('Protein Bars', 'Food', 10, 4),
  ('$50 Amazon Gift Card', 'Gift Cards', 50, 50);
  
 SELECT * FROM products;
--- bamazonManager.js
+++ bamazonManager.js
@@ -8,34 +8,162 @@ con.connect(function (err) {
   main();
 });
 
-var commands = ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Exit'];
+var commands = ['View Products For Sale', 'View Low Inventory', 'Add To Inventory', 'Add New Product', 'Exit'];
 
 function main() {
   log('');
   inquirer.prompt([
     {
       type: 'list',
-      message: chalk.bgGreen('Select command:'),
+      message: chalk.green.bold.underline('Select command:'),
       choices: commands,
       name: 'command',
     }
-  ]).then(function(answer){
-    switch(answer.command) {
-      case 'View Products for Sale':
-        // viewProducts();
+  ]).then(function (answer) {
+    switch (answer.command) {
+      case 'View Products For Sale':
+        viewProducts();
         break;
       case 'View Low Inventory':
-        // viewLowInventory();
+        viewLowInventory();
         break;
-      case 'Add to Inventory':
-        // addToInventory();
+      case 'Add To Inventory':
+        addToInventory();
         break;
       case 'Add New Product':
-        // addNewProduct();
+        addNewProduct();
         break;
       case 'Exit':
         log(chalk.cyan.bold('\nSigning off...'));
         process.exit(0);
+    };
+  });
+};
+
+function viewProducts() {
+  log(chalk.cyan.underline("\nProducts For Sale:\n"));
+  con.query('SELECT item_id, product_name, price, stock_quantity FROM products', function (err, res) {
+    if (err) throw err;
+    log(chalk.black.bgYellow("ID") + " | " + chalk.black.bgYellow('Name') + " | " + chalk.black.bgYellow("Price") + " | " + chalk.black.bgYellow("Quantity"));
+    for (var i = 0; i < res.length; i++) {
+      log(res[i].item_id + " | " + res[i].product_name + " | " + "$" + res[i].price + " | " + res[i].stock_quantity);
+    };
+    main();
+  });
+};
+
+function viewLowInventory() {
+  log(chalk.cyan.underline("\nLess than 5 units in stock:\n"));
+  con.query('SELECT item_id, product_name, price, stock_quantity FROM products WHERE stock_quantity<5', function (err, res) {
+    if (err) throw err;
+    log(chalk.black.bgYellow("ID") + " | " + chalk.black.bgYellow('Name') + " | " + chalk.black.bgYellow("Price") + " | " + chalk.black.bgYellow("Quantity"));
+    for (var i = 0; i < res.length; i++) {
+      log(res[i].item_id + " | " + res[i].product_name + " | " + "$" + res[i].price + " | " + res[i].stock_quantity);
+    };
+    main();
+  });
+};
+
+function addToInventory() {
+  con.query('SELECT item_id, product_name, stock_quantity FROM products', function (err, res) {
+    if (err) throw err;
+
+    var itemNames = [];
+    for (var i = 0; i < res.length; i++) {
+      itemNames.push(res[i].product_name);
+    };
+
+    inquirer.prompt([
+      {
+        type: 'list',
+        choices: itemNames,
+        message: 'Which product would you like to add inventory to?',
+        name: 'product'
+      }, {
+        type: 'input',
+        message: 'How many units to add?',
+        name: 'units',
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        },
+      }
+    ]).then(function (answer) {
+      var productToAdd = answer.product;
+      var amountToAdd = parseInt(answer.units);
+
+      con.query('UPDATE products SET ? WHERE ?', [
+        { stock_quantity: amountToAdd },
+        { product_name: productToAdd }
+      ], function (err, res) {
+        if (err) throw err;
+        log(chalk.bold.cyan(`\nAdded ${amountToAdd} units to ${productToAdd}.`));
+        main();
+      });
+    });
+  });
+};
+
+function addNewProduct() {
+  log('');
+  inquirer.prompt([
+    {
+      message: 'Name of new product:',
+      name: 'name',
+      validate: function (value) {
+        if (value == '') {
+          log(chalk.bgRed.white('ERROR:') + ' Please enter a name');
+          return false;
+        }
+        return true;
+      },
+    }, {
+      message: 'Price of new product:',
+      name: 'price',
+      validate: function (value) {
+        if (isNaN(value) === false) {
+          if (value == '') {
+            return false;
+          }
+          return true;
+        }
+        return false;
+      },
+    }, {
+      message: 'Starting inventory:',
+      name: 'inventory',
+      validate: function (value) {
+        if (isNaN(value) === false) {
+          if (value == '') {
+            return false;
+          }
+          return true;
+        }
+        return false;
+      },
+    }, {
+      message: 'Department of new product (optional):',
+      name: 'department',
+    }
+  ]).then(function (answer) {
+    var name = answer.name;
+    var price = parseInt(answer.price);
+    var inventory = parseInt(answer.inventory);
+    if (department == '') {
+      con.query(`INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("${name}", NULL, ${price}, ${inventory});`, function (err, res) {
+        if (err) throw err;
+        log(chalk.cyan.bold(`\nAdded ${inventory} units of ${name} at $${price} each.`));
+        main();
+      });
+    } else {
+      var department = answer.department;
+      con.query(`INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("${name}", "${department}", ${price}, ${inventory});`, function (err, res) {
+        if (err) throw err;
+        log(chalk.cyan.bold(`\nAdded ${inventory} units of ${name} at $${price} each.`));
+        main();
+      });
     };
   });
 };
